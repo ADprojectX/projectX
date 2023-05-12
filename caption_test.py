605050de09833745a6f8d0402665511f66a17f12
Aatish Dhami <32740715+Aatish-Dhami@users.noreply.github.com>
2023-05-11 21:21:14 -0700

    mini edit

--- caption_test.py
+++ caption_test.py
@@ -29,7 +29,7 @@ def align_audio_with_gentle(audio_file, text):
 
 def generate_captions(video_file, audio_file):
     # text = transcribe_audio(audio_file)
-    text = "The Placebo Effect occurs when a person experiences an improvement in their symptoms, even though they've received a treatment with no active ingredients. This demonstrates the power of our minds and how our beliefs can influence our physical health."
+    text = "Welcome to today's video, where we will explore fifteen psycological facts that will blow your mind. Let's dive right in."
     alignment = align_audio_with_gentle(audio_file, text)
     print(alignment)
     video = VideoFileClip(video_file)
@@ -51,8 +51,6 @@ def generate_captions(video_file, audio_file):
             text_clip = TextClip(word['word'], fontsize=28,  font=font_url, color='white', bg_color='transparent')
             text_width = text_clip.w
             # Composite the two clips together to create the final text clip
-            # final_clip = CompositeVideoClip([outline_clip.set_position((1, 1)), text_clip])
-            # Create a CompositeVideoClip with the width set to the text width
             final_clip = CompositeVideoClip([outline_clip.set_position((1, 1)), text_clip.set_position((3, 3))], size=(text_width + 10, text_clip.h + 10))
             final_clip = final_clip.set_start(start_time).set_end(end_time).set_position(('center', video.size[1]-50))
 
@@ -66,40 +64,3 @@ if __name__ == "__main__":
     video_file = "./sample_video.mp4"
     audio_file = "./sample_audio.wav"
     generate_captions(video_file, audio_file)
-
-
-
-# # Set the path of the audio and video files
-# audio_path = "./sample_audio.mp3"
-# video_path = "./sample_video.mp4"
-# transcript = "Welcome to today's video, where we will explore 15 psycological facts that will blow your mind. Let's dive right in."
-
-# # Load the video file
-# video = VideoFileClip(video_path)
-
-# words = transcript.split()
-
-# # Create a TextClip for each word in the transcript
-# text_clips = [TextClip(word, fontsize=20, color='white', bg_color='black')
-#               for word in words]
-
-# # Set the duration and start time for each TextClip
-# durations = [text_clip.duration for text_clip in text_clips]
-# start_times = [sum(durations[:i]) for i in range(len(durations))]
-
-# # Create a CompositeVideoClip with the TextClips
-# captioned_video = CompositeVideoClip([video] + text_clips, durations=durations)
-
-# # Set the start time for each TextClip
-# for i, text_clip in enumerate(text_clips):
-#     text_clip = text_clip.set_start(start_times[i])
-#     captioned_video = captioned_video.set_duration(start_times[-1])
-
-# # Write the captioned video to a file
-# video.write_videofile(
-#     "./captioned_video.mp4",
-#     codec="libx264",
-#     fps=24,
-#     audio=True,
-#     audio_codec="aac",
-#   )
